4bfb668d32696d999dd35051c569161985298483
EegArlert <bsyahputra@oakland.edu>
2024-09-03 00:13:00 -0400

    laying out pseudocode for the projects

---
+++ rock-paper-scissors.js
@@ -0,0 +1,12 @@
+//Rock paper scissors game step by step
+//Create function expression for computer choice and store it in computerChoice
+//Create a variable humanChoice that accepts human choice 
+//Create a function that validate the winner from computerChoice and humanChoice
+    //whoever won, their score +1
+/*
+Then, write logics for these two objectives:
+
+    1. Write the logic to play a single round, incremenets the round winner's score and logs a winner announcement
+
+    2. Write the logic to play the game for five rounds, keep track of scores each round, and declare winner at the end.
+*/
